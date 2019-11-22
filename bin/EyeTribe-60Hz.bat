a9eb3c061ca6f1ab8f4e4c2be014707d9c83f028
lucas137 <52358086+lucas137@users.noreply.github.com>
2019-11-22 17:33:12 -0500

    Add files via upload

---
+++ bin/EyeTribe-60Hz.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM   Usage:
+REM
+REM   EyeTribe [<config file>] [--help (-?)] {options},
+REM   where options is zero or more of:
+REM
+REM   --port=<tcp port number> (-p)
+REM   --remote=<bool> (-r)
+REM   --device=<device number> (-d)
+REM   --framerate=<30|60>  (-f)
+
+start "EyeTribe server" "C:\Program Files (x86)\EyeTribe\Server\EyeTribe.exe" --framerate=60
