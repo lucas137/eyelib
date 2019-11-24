28189883d504790c0003a91c61f7abbf3bd5c3d3
lucas137 <52358086+lucas137@users.noreply.github.com>
2019-11-24 18:12:52 -0500

    Add files via upload

--- test/src/eyelib_test.cpp
+++ test/src/eyelib_test.cpp
@@ -1,6 +1,26 @@
 //===========================================================================//
-//  Nathan Lucas
-//  2016
+/*  MIT License
+
+Copyright (c) 2019 Nathan Lucas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 //===========================================================================//
 
 #include "test_fixation.hpp"    // eye::test::fixation
@@ -22,6 +42,9 @@ void
 print_usage(std::string const& name)
 {
   std::cout
+    << "\nEyelib version " << eye::version << " ("
+    <<  eye::build_date("-") << "T" <<  eye::build_time(":") << ")"
+    << '\n'
     << "\nEye tracking library test."
     << '\n'
     << "\n  " << name << "  test [-s:N]"
