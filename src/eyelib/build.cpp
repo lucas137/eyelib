17041da79ebc837f84fcee3af11a56bb714b5fba
lucas137 <52358086+lucas137@users.noreply.github.com>
2019-11-24 18:11:59 -0500

    Add files via upload

--- src/eyelib/build.cpp
+++ src/eyelib/build.cpp
@@ -22,62 +22,28 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 //===========================================================================//
-/// @file
-/// @brief    Window text.
-/// @author   Nathan Lucas
-//===========================================================================//
-#ifndef EYLIB_WINDOW_TEXT_HPP
-#define EYLIB_WINDOW_TEXT_HPP
 
-#include <array>      // std::array
+#include <eyelib.hpp>
+
+#include <utl/compile.hpp>
+
 #include <string>     // std::string
 
-namespace eye { namespace window {
-
-/// @ingroup    window
-/// @addtogroup text  Window Text
-/// @{
-
+namespace eye {
 //---------------------------------------------------------------------------
 
-struct TextLine       ///< Single line of text.
+std::string
+build_date(std::string const& delim)
 {
-  std::string str{};  ///< String containing text to draw.
-  int x_px{0};        ///< Horizontal position in pixels.
-  int y_px{0};        ///< Vertical position of text in pixels.
-  TextLine(std::string const& str, int x_px, int y_px);
-  TextLine() = default;
-};
+  return utl::compile::date_yyyymmdd_str("-");
+}
 
-//---------------------------------------------------------------------------
-
-struct TextWidget     ///< Three lines of text.
+std::string
+build_time(std::string const& delim)
 {
-  bool show{true};    ///< `true` to draw on screen.
-
-  /** @brief  Construct text lines.
-
-    Example:
-    ```
-    Text t(scr.w_px/2, scr.h_px/3, "Eye Tracker Application");`
-    ```
-  */
-  TextWidget(int x_px, int y_px, std::string const& intro);
-
-  TextWidget() = default;   ///< Default constructor.
-  void draw() const;        ///< Draw lines of text.
-
-private:
-  static constexpr std::size_t size_ = 5;
-  int cx_{0};     // center of screen
-  std::array<TextLine, size_> lines_{};
-};
+  return utl::compile::time_hhmmss_str(":");
+}
 
 //---------------------------------------------------------------------------
-
-/// @}
-
-} } // eye::window
-
-#endif // EYLIB_WINDOW_TEXT_HPP
+} // eye
 //===========================================================================//
--- src/eyelib/tracker/tracker.cpp
+++ src/eyelib/tracker/tracker.cpp
@@ -1,6 +1,26 @@
 //===========================================================================//
-//  Nathan Lucas
-//  2016
+/*  MIT License
+
+Copyright (c) 2019 Nathan Lucas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 //===========================================================================//
 
 #include <eyelib.hpp>
@@ -12,7 +32,6 @@
 #include "window/window.hpp"
 
 #include <utl/asio/tcp/client.hpp>  // utl::io::tcp::server
-#include <utl/compile.hpp>          // utl::compile::date_yyyymmdd
 #include <utl/json.hpp>             // nlohmann::json
 #include <utl/memory.hpp>           // utl::make_unique
 #include <utl/string.hpp>           // utl::parse
@@ -343,16 +362,10 @@ Tracker::Impl::process_tracker_response(tracker::Message const& m)
 Tracker::Tracker(std::string const& host, std::string const& port,
                  Screen const& scr)
 : pimpl(utl::make_unique<Impl>(host, port, scr))
-{
-  std::cout << "eyelib " << version
-            << " (" << utl::compile::date_yyyymmdd_str("-") << ")"
-            << std::endl;
-}
+{}
 
 Tracker::~Tracker()
-{
-  std::cout << "eyelib: exit" << std::endl;
-}
+{}
 
 //---------------------------------------------------------------------------
 
